3731f72e3a7afddfc9cc6ac17346cc6a327f973d
Christophe Hurpeau <christophe@hurpeau.com>
2018-07-06 21:12:12 +0200

    feat: pob update, export default for typescript

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,3 @@
-declare module "parse-json-object-as-map" {
-  declare function parseJsonObjectAsMap(text: string, reviver?: (key: any, value: any) => any): any
-  export = parseJsonObjectAsMap;
-}
+declare function parseJsonObjectAsMap(text: string, reviver?: (key: any, value: any) => any): any
+export default parseJsonObjectAsMap;
 
